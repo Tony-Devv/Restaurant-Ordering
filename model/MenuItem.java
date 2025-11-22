56fedd333be971bb5c3d5d0cc39f1f7c4d315234
hatem32 <152395771+hatem32@users.noreply.github.com>
2025-11-22 20:23:25 +0200

    Refactor MenuItem interface structure
    Removed unnecessary package declaration and added a newline at the end of the file.

--- model/MenuItem.java
+++ model/MenuItem.java
@@ -3,5 +3,6 @@ package model;
 public interface MenuItem {
     String getDescription();
     double getPrice();
-    String getCategory(); // For discount calculation (Hatem use This for Discount
+    String getCategory(); 
+
 }
